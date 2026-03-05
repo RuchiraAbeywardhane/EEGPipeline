c4ddf8fd30807537fc73153fcfdf593c1d1c11e6
RuchiraAbeywardhane <ruchiraabeywardhane08@gmail.com>
2026-03-05 13:03:12 +0530

    updated

--- eeg_config.py
+++ eeg_config.py
@@ -13,8 +13,8 @@ import torch
 class Config:
     """EEG-specific configuration."""
     # Paths
-    DATA_ROOT = "/kaggle/input/datasets/nethmitb/emognition-processed/Output_KNN_ASR"
-    # DATA_ROOT = "/kaggle/input/datasets/ruchiabey/emognition"
+    # DATA_ROOT = "/kaggle/input/datasets/nethmitb/emognition-processed/Output_KNN_ASR"
+    DATA_ROOT = "/kaggle/input/datasets/ruchiabey/emognition"
     # DATA_ROOT = "/kaggle/input/datasets/ruchiabey/asr-outputv2-0/ASR_output"
     
     # Common parameters
